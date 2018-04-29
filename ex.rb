419b18bf6bb290894c28a547d52a906002e2da9a
fatKitchen <winterwonderland@zeus.eonet.ne.jp>
2018-04-29 18:42:44 +0900

    8.2.6 class method

--- ex.rb
+++ ex.rb
@@ -1,17 +1,25 @@
 class HelloWorld
+  attr_accessor :name
   def initialize(myname = "Ruby")
     @name = myname    #インスタンス変数の初期化
   end
 
-  def hello
-    puts "Hello, world. I am #{@name}."
+  def greet
+    puts "Hi, I am #{self.name}." # self.nameと@nameの違いは？？
   end
 end
+
+class << HelloWorld
+  def hello(name)
+    puts "#{name} said hello."
+  end
+end
+
 
 bob = HelloWorld.new("Bob")
 alice = HelloWorld.new("Alice")
 ruby = HelloWorld.new
 
-bob.hello
+bob.greet
 
-
+HelloWorld.hello("John")
